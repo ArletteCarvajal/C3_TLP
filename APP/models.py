eac5530d5658bb852c280c12d099a7593e84a218
ArletteCarvajal <95262903+ArletteCarvajal@users.noreply.github.com>
2024-07-06 18:02:12 -0400

    Ajustes para el administrador
    Tablas y detalles de está para que el administrador pueda realizar sus funciones de crear, editar y eliminar plantas y productos

--- APP/models.py
+++ APP/models.py
@@ -3,20 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Operador (models.Model):  #modelo solo con los nombres de los operadores
-        nombre = models.CharField(max_length = 100)
-        #funcion para que en las tablas de la BBDD se vean los nombres y no como object
-        def str(self) -> str:
-                return self.nombre
+        nombre_operador = models.CharField(max_length = 100)
 
+        #funcion para que en las tablas de la BBDD se vean los nombres y no como object
+        def __str__(self):
+                return self.nombre_operador
 
 class Plantas (models.Model):    #modelo de plantas con sus nombres y contraccion
-        nomnbre = models.CharField(max_length = 100)
-        codigo = models.CharField(max_length = 100)
+        nombre_planta = models.CharField(max_length = 100)
+        codigo_planta = models.CharField(max_length = 100)
 
 class Productos (models.Model): #modelo de productos, contraccion o codigo y la planta de donde proviene
-        nombre = models.CharField(max_length = 100)
-        codigo = models.CharField(max_length = 3, primary_key = True)
-        planta = models.ForeignKey(Plantas, on_delete = models.CASCADE)
+        nombre_producto = models.CharField(max_length = 100)
+        codigo_producto = models.CharField(max_length = 3, primary_key = True)
+        nombre_planta = models.ForeignKey(Plantas, on_delete = models.CASCADE)
+
+        def _str_(self):
+                return f'{self.nombre_producto} - {self.codigo_producto} - {self.nombre_planta.nombre_planta}'
 
 class Registro_Produccion (models.Model):
         codigo_combustible = models.ForeignKey(Productos, on_delete = models.CASCADE)
@@ -26,3 +29,5 @@ class Registro_Produccion (models.Model):
         hora_registro = models.TimeField()
         operador = models.ForeignKey(Operador, on_delete = models.CASCADE)
 
+        def _str_(self):
+                return f'{self.codigo_combustible.codigo_producto} - {self.litros_produccion} - {self.fecha_produccion} - {self.turno} - {self.hora_registro} - {self.operador.nombre_operador}'
